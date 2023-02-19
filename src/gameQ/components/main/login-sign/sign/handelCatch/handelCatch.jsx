b173c1ed3de5307c3d5268c3a578cfd9239a0f9e
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-19 04:30:02 -0800

    Catch error and show that in alert box , that form is made by React-bootstrap

---
+++ src/gameQ/components/main/login-sign/sign/handelCatch/handelCatch.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+
+export function HandelCatch({massage}) {
+    const [show, setShow] = useState(true);
+
+    if (show) {
+        return (
+            <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+                <Alert.Heading>{massage}</Alert.Heading>
+                <p>
+                    CAN NOT SIGN UP , THERE IS A PROBLEM . IF YOU LOGINED IN PAST YOU SHOULD DELETE YOUR BROWSER
+                    LOCALSTORAGE FROM SETTING
+                </p>
+            </Alert>
+        );
+    }
+    return <Button onClick={() => setShow(true)}>Show Alert</Button>;
+}
