c0f037d8f0f5aba518007999dd1ff7d5f4a74d77
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-19 04:36:53 -0800

    Send and receive request by axios library

---
+++ src/gameQ/components/main/login-sign/operationOfRequest/axiosOperation.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export let FetchPost = async (information) => {
+
+    return axios.post("http://localhost:3001/post", information)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+}
