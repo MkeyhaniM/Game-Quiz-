0e98ecf0d2dad9f39733dda28fc98ee87fee8ac8
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-19 04:25:06 -0800

    Create a localserver by express . The information that is posted there

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 3001;
+const app = express();
+let userLogged = [];
+
+const token = "GameQuiz";
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/post', (req, res) => {
+    let {information} = req.body;
+    if (information) {
+        res.send({
+            success: true,
+            data: information,
+            token,
+            error: false
+        })
+        userLogged.push(information)
+    } else {
+        res.send({
+            success: false,
+            data: req.body,
+            token: null,
+            error: true,
+            mass: req.body
+        })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Connect the local server By localhost://${port} `)
+})
