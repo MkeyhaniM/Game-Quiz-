e1899c632fa403710682872253f24c7fb339d07f
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-19 04:21:11 -0800

    throw form for access to last values of state and dispatcher

--- src/gameQ/components/main/main-page.jsx
+++ src/gameQ/components/main/main-page.jsx
@@ -1,13 +1,16 @@
-import React, { Fragment } from "react";
+import React, {Fragment} from "react";
 
-import { FormLoginSign } from "./login-sign/index";
-import { Title } from "./titleOfGameQuiz/title";
+import {FormLoginSign} from "./login-sign/index";
+import {Title} from "./titleOfGameQuiz/title";
+import {ContextOfGameQuiz} from "./context/contextGameQuiz";
 
 export function MainPage() {
-  return (
-    <Fragment>
-      <Title />
-      <FormLoginSign />
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Title/>
+            <ContextOfGameQuiz>
+                <FormLoginSign/>
+            </ContextOfGameQuiz>
+        </Fragment>
+    );
 }
