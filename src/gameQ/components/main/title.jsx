607ba379a8adea6e45c61bd291fa9c79d1510f06
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-18 22:28:26 -0800

    title of my page

--- src/gameQ/components/main/title.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function Title() {
-  return <h2 className="mt-5 mb-5 text-primary p-2 text text-center">
-    Game Quiz
-  </h2>;
-}
---
+++ src/gameQ/components/main/titleOfGameQuiz/title.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Title() {
+  return <h2 className="mt-5 mb-5 text-primary p-2 text text-center">
+    Game Quiz
+  </h2>;
+}
