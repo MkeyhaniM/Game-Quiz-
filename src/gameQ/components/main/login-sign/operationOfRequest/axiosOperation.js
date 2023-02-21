5985b2a2767c857d54d04c7a1bdc7f32c651f25b
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-21 10:07:17 -0800

    send as an object

--- src/gameQ/components/main/login-sign/operationOfRequest/axiosOperation.js
+++ src/gameQ/components/main/login-sign/operationOfRequest/axiosOperation.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export let FetchPost = async (information) => {
 
-    return axios.post("http://localhost:3001/post", information)
+    return axios.post("http://localhost:3001/post", {information})
         .then(response => response.data)
         .catch(error => console.log(error));
 }
