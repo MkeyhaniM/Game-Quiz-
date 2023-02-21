103986ce89171326489d54b90586f3d63549038c
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-21 10:12:00 -0800

    Space

--- src/gameQ/components/main/login-sign/login-sign.jsx
+++ src/gameQ/components/main/login-sign/login-sign.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { FormLogin } from "./login/login";
-import { FormSign } from "./sign/sign";
+import {FormLogin} from "./login/login";
+import {FormSign} from "./sign/sign";
 
 function checkUserLogined() {
-  if (localStorage.getItem("Game-quiz")) {
-    return true;
-  } else {
-    return false;
-  }
+    if (localStorage.getItem("Game-quiz")) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 export function FormLoginSign() {
-  const check = checkUserLogined() ? <FormLogin /> : <FormSign />;
-
-  return <div>{check}</div>;
+    const check = checkUserLogined() ? <FormLogin/> : <FormSign/>;
+    return <div>{check}</div>;
 }
  
