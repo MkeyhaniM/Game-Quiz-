b1cbf891c0d60637f0b35d73eda948f712d45c40
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-22 13:10:22 -0800

    Button next Quiz

---
+++ src/gameQ/components/game/creatQuiz/nextQuiz/nextQuiz.jsx
@@ -0,0 +1,9 @@
+import Button from 'react-bootstrap/Button';
+
+export function NextQuiz() {
+    return (
+        <>
+            <Button className='mt-4' variant="primary">Next Quiz</Button>{' '}
+        </>
+    );
+}
