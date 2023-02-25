443017ded06b7c49dc8b724416a84791f9a03752
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-25 08:30:10 -0800

    Highlighting correct answer

--- src/gameQ/components/game/creatQuiz/Options/options.jsx
+++ src/gameQ/components/game/creatQuiz/Options/options.jsx
@@ -3,32 +3,45 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-export function Options({Options}) {
+export function Options({Options, getCorrect}) {
+    function check(e) {
+        switch (e) {
+            case true:
+                return 'bg-success';
+                break;
+            case false:
+                return 'bg-danger';
+                break;
+            default:
+                return null
+        }
+    }
+
     return (
         <Container>
             <Row>
-                <Col className='p-2 m-1 border border-opacity-10'>
-                    <Badge bg="secondary">
+                <Col className={`p-2 m-1 border border-opacity-10 ${check(getCorrect)} `}>
+                    <Badge style={{fontSize: '15px'}} bg="secondary">
                         A
                     </Badge>{' '}
                     {Options[0]}
                 </Col>
-                <Col className='p-2 m-1 border border-opacity-10'>
-                    <Badge bg="secondary">
+                <Col className={`p-2 m-1 border border-opacity-10 ${check(getCorrect)}`}>
+                    <Badge style={{fontSize: '15px'}} bg="secondary">
                         B
                     </Badge>{' '}
                     {Options[1]}
                 </Col>
             </Row>
             <Row>
-                <Col className='p-2 m-1 border border-opacity-10'>
-                    <Badge bg="secondary">
+                <Col className={`p-2 m-1 border border-opacity-10 ${check(getCorrect)}`}>
+                    <Badge style={{fontSize: '15px'}} bg="secondary">
                         C
                     </Badge>{' '}
                     {Options[2]}
                 </Col>
-                <Col className='p-2 m-1 border border-opacity-10'>
-                    <Badge bg="secondary">
+                <Col className={`p-2 m-1 border border-opacity-10 ${check(getCorrect)}`}>
+                    <Badge style={{fontSize: '15px'}} bg="secondary">
                         D
                     </Badge>{' '}
                     {Options[3]}
@@ -37,6 +50,7 @@ export function Options({Options}) {
         </Container>
     );
 }
+
 // export function OptionsALL() {
 //     return (
 //         <div>
