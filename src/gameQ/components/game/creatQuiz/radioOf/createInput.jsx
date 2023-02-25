120e94667fbd99abf3870c7318004e84bd58efe0
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-25 08:33:32 -0800

    Add unique data-index

--- src/gameQ/components/game/creatQuiz/radioOf/createInput.jsx
+++ src/gameQ/components/game/creatQuiz/radioOf/createInput.jsx
@@ -1,16 +1,19 @@
 import Form from "react-bootstrap/Form";
 import Col from 'react-bootstrap/Col';
-export function Radio({name, type}) {
+export function Radio({name,check,index}) {
     return (
         <>
-            <Col>
-                <label className='fw-bold' style={{fontSize:'22px'}} htmlFor={`Radio___${name}`}>
+            <Col className='text text-center'>
+                <label className='fw-bold' style={{fontSize:'20px'}} htmlFor={`Radio___${name}`}>
+                    {name} :
                     <Form.Check
+                        onChange={e=>check(e)}
+                        className='px-2'
                         inline
+                        data-index={index}
                         name="group1"
                         type='radio'
-                        id={`inline-${name}`}/>
-                    {name}:
+                        id={`Answer${index}`}/>
                 </label>
             </Col>
         </>
