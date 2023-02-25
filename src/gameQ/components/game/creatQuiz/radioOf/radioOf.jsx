b96db1a2cf00db53e0aa328a4d7391ec2acd88b8
mkeyhanim <mkeyhanideve@gmail.com>
2023-02-25 08:28:04 -0800

    background for radios

--- src/gameQ/components/game/creatQuiz/radioOf/radioOf.jsx
+++ src/gameQ/components/game/creatQuiz/radioOf/radioOf.jsx
@@ -3,15 +3,15 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
 
-export function RadioOf() {
+export function RadioOf({selectedClicetAnswer}) {
     var unciqueKey = 0;
     return (
         <>
-            <div style={{width: '1100px', borderRadius: '7px'}} className='container bg-primary p-3 my-4'>
+            <div style={{width: '1100px', borderRadius: '7px'}} className='container bg-secondary bg-opacity-25 p-3 my-4'>
                 <Container>
                     <Row>
                         {["A", "B", "C", "D"].map((e, type, index) => {
-                            return <Radio key={unciqueKey += 1} type={index} name={e}/>
+                            return <Radio key={unciqueKey += 1} check={selectedClicetAnswer} index={unciqueKey} name={e}/>
                         })}
                     </Row>
                 </Container>
