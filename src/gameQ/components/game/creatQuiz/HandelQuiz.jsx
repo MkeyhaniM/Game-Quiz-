48380b7034e54db9e1bf3e4eff518d9b144eec26
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-02 11:43:57 -0800

    Deleted useStates and get value by context.

--- src/gameQ/components/game/creatQuiz/HandelQuiz.jsx
+++ src/gameQ/components/game/creatQuiz/HandelQuiz.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useContext} from "react";
 import {Quiz} from "../../../quiz/quiz";
 import {shuffle} from "../mixQuiz/mixQuestionAndAnswers";
 import {CreateQ} from "./quiz/quizQuestion"
@@ -9,60 +9,41 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import {RadioOf} from "./radioOf/radioOf";
+import {pro} from "../game";
 
-var shuffledQuestionArray = shuffle(Quiz)
-var counterQuestion = 0;
-
-
-function showAnswerInOptions(ICR, CA, SAA) {
-    for (var i = 0; i < SAA.length; i++) {
-        if (SAA[ICR] === CA) {
-            console.log("hello")
-            return (<Options getCorrect={true} Options={SAA[ICR]}/>)
-            break;
-        } else {
-            console.log("bye")
-            return (<Options getCorrect={false} Options={SAA}/>)
-            break;
-        }
-    }
-}
 
 function Q() {
-    const [indexClickedRadio, setIndexClickedRadio] = useState(-1);
-    const [condition, setCondition] = useState(false);
-    const [selectedQuiz, setSelectedQuiz] = useState(shuffledQuestionArray[counterQuestion]);
-    const [conrrectAnswer, setConrrectAnswer] = useState(selectedQuiz.Answer);
-    const [shuffledAnswerArray, setShuffledAnswerArray] = useState(shuffle(selectedQuiz.Test));
-    var DA = selectedQuiz.DescriptionAboutAnswer ? selectedQuiz.DescriptionAboutAnswer : null;
+    const context = useContext(pro);
+    var DA = context.selectedQuiz.DescriptionAboutAnswer ? context.selectedQuiz.DescriptionAboutAnswer : null;
+    const [indexClickedRadio, setIndexClickedRadio] = useState(null);
 
-    function CheckAnwser(e) {
+    function CheckAnswer(e) {
         setIndexClickedRadio(Number(e.target.getAttribute('data-index')));
-        setCondition(true)
     }
 
-    return (<>
-        <CreateQ counterQuestion={counterQuestion + 1} name="mahyarKeyhani" question={selectedQuiz.Question}/>
-
-        {condition && showAnswerInOptions(indexClickedRadio, conrrectAnswer, shuffledAnswerArray)}
-        {!condition && <Options getCorrect={null} Options={shuffledAnswerArray}/>}
-
-        <RadioOf selectedClicetAnswer={CheckAnwser}/>
-        <Container>
-            <Row>
-                <Col>
-                    <NextQuiz/>
-                </Col>
-                <Col>
-                    <DescriptionOfQuestion DescriptionAnwser={DA}/>
-                </Col>
-            </Row>
-        </Container>
-    </>)
+    return (
+        <>
+            <CreateQ counterQuestion={context.counter + 1} name="Mahyar-Keyhani"
+                     question={context.selectedQuiz.Question}/>
+            <Options Options={context.shuffledTests} check={indexClickedRadio} Answer={context.Answer}/>
+            <RadioOf selectedClicetAnswer={CheckAnswer}/>
+            <Container>
+                <Row>
+                    <Col>
+                        <NextQuiz/>
+                    </Col>
+                    <Col>
+                        <DescriptionOfQuestion DescriptionAnwser={DA}/>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
 }
 
 export function HandelQuiz() {
-    return (<div className={`container Quiz${counterQuestion}`}>
+    const context = useContext(pro);
+    return (<div className={`container Quiz${context.counter}`}>
         <Q/>
     </div>)
 }
