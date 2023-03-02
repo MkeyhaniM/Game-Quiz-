44df1f34aae43a68b53011184ab026adeaed3d01
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-02 11:45:37 -0800

    Create Context and create some states to get details of quiz.

--- src/gameQ/components/game/game.jsx
+++ src/gameQ/components/game/game.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useState, createContext} from "react";
 import {HandelQuiz} from "./creatQuiz/HandelQuiz";
+import {Quiz} from "../../quiz/quiz";
+import {shuffle} from "./mixQuiz/mixQuestionAndAnswers"
+
+export const pro = createContext(null)
 
 export function Game_quiz() {
+    const [sh_array, setSh_array] = useState(shuffle(Quiz));
+    const [counterQuestion, setCounterQuestion] = useState(0);
+    const [selectedQuiz, setSelectedQuiz] = useState(sh_array[counterQuestion]);
+    const sendInformationQuiz = {selectedQuiz, shuffledTests: shuffle(selectedQuiz.Test), counter: counterQuestion,Answer: selectedQuiz.Answer}
+
+
     return (
-        <div><HandelQuiz/></div>
+        <pro.Provider value={sendInformationQuiz}>
+            <HandelQuiz qc={counterQuestion}/>
+        </pro.Provider>
     )
 }
