bbf0e8517ed9e4fd610fd7a33d33db3b55840ecc
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-02 11:49:32 -0800

    Modify a props and create accordion to show description about quiz.

--- src/gameQ/components/game/creatQuiz/descriptionOfQuestion/descriptionOfQuestion.jsx
+++ src/gameQ/components/game/creatQuiz/descriptionOfQuestion/descriptionOfQuestion.jsx
@@ -1,13 +1,15 @@
 import Accordion from 'react-bootstrap/Accordion';
 import Badge from "react-bootstrap/Badge";
 
-export function DescriptionOfQuestion({DescriptionAnwser}) {
+export function DescriptionOfQuestion({DescriptionAnswer}) {
     return (
         <Accordion className='mt-4' defaultActiveKey="0">
             <Accordion.Item eventKey="1">
-                <Accordion.Header>DescriptionOfQuestion : {DescriptionAnwser ? <Badge className='m-1' bg="primary" pill>1</Badge> :<Badge className='m-1' bg="dark" pill>0</Badge>} </Accordion.Header>
+                <Accordion.Header>DescriptionOfQuestion : {DescriptionAnswer ?
+                    <Badge className='m-1' bg="primary" pill>1</Badge> :
+                    <Badge className='m-1' bg="dark" pill>0</Badge>} </Accordion.Header>
                 <Accordion.Body>
-                    {DescriptionAnwser ? DescriptionAnwser : "There is no description"}
+                    {DescriptionAnswer ? DescriptionAnswer : "There is no description"}
                 </Accordion.Body>
             </Accordion.Item>
         </Accordion>
