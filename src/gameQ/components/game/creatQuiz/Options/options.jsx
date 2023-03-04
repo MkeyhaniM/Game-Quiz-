02cfbbf18e691c009cb3fafd42e409a5e408ef74
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-04 05:09:12 -0800

    Finally , it had done . show correct answer , or it is chosen if it is wrong client get correct answer.

--- src/gameQ/components/game/creatQuiz/Options/options.jsx
+++ src/gameQ/components/game/creatQuiz/Options/options.jsx
@@ -1,36 +1,44 @@
+import {Fragment} from "react";
 import Badge from 'react-bootstrap/Badge';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import React, {Fragment, useState} from "react";
-
 
 export function Options(props) {
     const word = ['A', 'B', 'C', 'D']
-    const background = ['bg-success', 'bg-danger', 'bg-light']
+    const backgroundDefault = ['bg-success', 'bg-danger', 'bg-light']
 
-    function CreateOption({CounterWord, index, background, det, isCorrect}) {
-
+    function CreateOption({CounterWord, index, background, isCorrect}) {
         function getCorrectOption() {
-            if (isCorrect) {
+            if (isCorrect == null) {
                 return (
                     <Fragment>
-                        <Col className={`p-2 m-1 border border-opacity-10 bg-opacity-100 ${background} `}>
-                            <Badge style={{fontSize: '15px'}} bg="secondary">
-                                {CounterWord}
+                        <Col className={`p-2 m-1 border border-opacity-10 bg-opacity-100 text-primary ${background} `}>
+                            <Badge className='bg-opacity-25' style={{fontSize: '15px'}} bg="secondary">
+                                <span className='text-primary'>{CounterWord}</span>
                             </Badge>{' '}
                             {props.Options[index]}
                         </Col>
                     </Fragment>
                 )
             } else if (!isCorrect) {
-                console.log(det)
-                if (props.Options[index] !== det.first) {
-                    console.log('hello')
-                }
+                const assignDetailsFromWindow = window.keepDetails
                 return (
                     <Fragment>
-                        <Col className={`p-2 m-1 border border-opacity-10 bg-opacity-100 ${background} `}>
+                        <Col
+                            className={`p-2 m-1 border border-opacity-10 bg-opacity-100$ ${assignDetailsFromWindow[1] === index ? backgroundDefault[0] : background}  `}>
+                            <Badge style={{fontSize: '15px'}} bg="secondary">
+                                {CounterWord}
+                            </Badge>{' '}
+                            {props.Options[index]}
+                        </Col>
+                    </Fragment>)
+            } else if (isCorrect) {
+                console.log('level' + index)
+                return (
+                    <Fragment>
+                        <Col
+                            className={`p-2 m-1 border border-opacity-10 bg-opacity-100 text-bg-warning ${backgroundDefault[0]} `}>
                             <Badge style={{fontSize: '15px'}} bg="secondary">
                                 {CounterWord}
                             </Badge>{' '}
@@ -43,59 +51,49 @@ export function Options(props) {
 
         return (
             <Fragment>
-                {/*<Col className={`p-2 m-1 border border-opacity-10 bg-opacity-100 ${background} `}>*/}
-                {/*    <Badge style={{fontSize: '15px'}} bg="secondary">*/}
-                {/*        {CounterWord}*/}
-                {/*    </Badge>{' '}*/}
-                {/*    {props.Options[index]}*/}
-                {/*</Col>*/}
                 {getCorrectOption()}
             </Fragment>
         )
     }
 
-    console.log(props.Answer)
+    function Details(option, ca) {
+        word.map((e, index) => {
+            if (option[index] === ca) {
+                window.keepDetails = [option[index], index]
+            }
+        })
+    }
 
     function CheckTypeOfOperation() {
-
+        Details(props.Options, props.Answer)
         if (props.check == null) {
             return word.map((Op, index) => {
-                return (
-                    <Fragment key={index + Op}>
-                        <CreateOption CounterWord={Op} backround={background[2]} index={index}/>
-                    </Fragment>
-                )
+                return (<Fragment key={index + Op}>
+                    <CreateOption isCorrect={null} CounterWord={Op} backround={backgroundDefault[2]} index={index}/>
+                </Fragment>)
             })
         } else {
-            const getCorrectOption = props.Options.map((get_C_Option, indexGetCorrectOp) => {
-                if (props.Options[indexGetCorrectOp] == props.Answer) {
-                    return {details: {first: indexGetCorrectOp, second: props.Answer}}
-                }
-            })
             return word.map((Op, index) => {
                 if (props.Options[props.check] === props.Answer && index === props.check) {
                     return (
                         <Fragment key={index + Op}>
-                            <CreateOption isCorrect={true} CounterWord={Op} background={background[0]} index={index}/>
+                            <CreateOption isCorrect={true} CounterWord={Op} background={backgroundDefault[0]}
+                                          index={index}/>
                         </Fragment>
                     )
                 } else if (props.Options[props.check] !== props.Answer && index === props.check) {
-                    var details;
-                    getCorrectOption.forEach(e => {
-                        if (e) {
-                            details = e
-                        }
-                    })
+
                     return (
                         <Fragment key={index + Op}>
-                            <CreateOption isCorrect={false} CounterWord={Op} det={details} background={background[1]}
+                            <CreateOption isCorrect={false} CounterWord={Op} background={backgroundDefault[1]}
                                           index={index}/>
                         </Fragment>
                     )
                 } else {
                     return (
                         <Fragment key={index + Op}>
-                            <CreateOption isCorrect={false} CounterWord={Op} background={background[2]} index={index}/>
+                            <CreateOption isCorrect={false} CounterWord={Op} background={backgroundDefault[2]}
+                                          index={index}/>
                         </Fragment>
                     )
                 }
@@ -104,12 +102,10 @@ export function Options(props) {
     }
 
     return (
-
         <Container>
             <Row>
                 <CheckTypeOfOperation/>
             </Row>
         </Container>
     );
-
 }
