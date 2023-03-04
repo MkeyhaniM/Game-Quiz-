c5d82fd3dbc6135744a4215cdf538fc87de220f4
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-04 10:52:50 -0800

    Disabled radios when user Clicked one of them.

--- src/gameQ/components/game/creatQuiz/radioOf/radioOf.jsx
+++ src/gameQ/components/game/creatQuiz/radioOf/radioOf.jsx
@@ -3,15 +3,17 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
 
-export function RadioOf({selectedClicetAnswer}) {
+export function RadioOf({selectedClientAnswer, getDisabled}) {
     var unciqueKey = 0;
+    const word = ["A", "B", "C", "D"];
     return (
         <>
-            <div style={{width: '1100px', borderRadius: '7px'}} className='container bg-secondary bg-opacity-25 p-3 my-4'>
+            <div style={{width: '1100px', borderRadius: '7px'}}
+                 className='container bg-secondary bg-opacity-25 p-3 my-4'>
                 <Container>
                     <Row>
-                        {["A", "B", "C", "D"].map((e, type, index) => {
-                            return <Radio key={unciqueKey += 1} check={selectedClicetAnswer} index={unciqueKey} name={e}/>
+                        {word.map((e) => {
+                            return <Radio key={unciqueKey += 1} getDisabled={getDisabled} check={selectedClientAnswer} index={unciqueKey} name={e}/>
                         })}
                     </Row>
                 </Container>
