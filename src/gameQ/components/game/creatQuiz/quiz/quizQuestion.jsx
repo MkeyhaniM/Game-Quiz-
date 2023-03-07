666d6ecf14cda315b953e54b8d5ef134d9cf0fd7
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-07 05:41:55 -0800

    Deleted props

--- src/gameQ/components/game/creatQuiz/quiz/quizQuestion.jsx
+++ src/gameQ/components/game/creatQuiz/quiz/quizQuestion.jsx
@@ -2,12 +2,12 @@ import Alert from 'react-bootstrap/Alert';
 import Badge from "react-bootstrap/Badge";
 import React from "react";
 
-export function CreateQ({question, name, counterQuestion}) {
+export function CreateQ({question }) {
     return (
         <Alert variant="secondary">
             {/*<Alert.Heading>Hey, {name}</Alert.Heading>*/}
             <Badge className='d-inline-block m-2' bg="secondary">
-                {counterQuestion}
+                Q
             </Badge>
             <p className='d-inline f'>
                 {question}
