1c6226680573b04ea80c05b25f4fc2ae614f804c
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-09 12:29:38 -0800

    Context for form login sign

--- src/gameQ/components/context/contextGameQuiz.jsx
+++ src/gameQ/components/context/contextGameQuiz.jsx
@@ -1,10 +1,10 @@
-import React, {createContext, useContext, useReducer} from "react";
+import {createContext, useContext, useReducer} from "react";
 import {InitialState} from "./initialState";
 import {Reducer} from "./reducer";
 import {Game_quiz} from "../game/index";
 
-const ContextOfState = createContext(null)
-const ContextOfDispatch = createContext(null)
+const ContextOfState = createContext(null);
+const ContextOfDispatch = createContext(null);
 
 export let ContextStateOfReducer = () => {
     const context = useContext(ContextOfState);
@@ -25,11 +25,10 @@ export let ContextDispatchOfReducer = () => {
 
 export function ContextOfGameQuiz({children}) {
     const [state, dispatch] = useReducer(Reducer, InitialState);
-    // const {token} = state;
+    const {token} = state;
     return (<ContextOfState.Provider value={state}>
         <ContextOfDispatch.Provider value={dispatch}>
-            {/*{token ? <Game_quiz/> : children}*/}
-            <Game_quiz/>
+            {token ? <Game_quiz/> : children}
         </ContextOfDispatch.Provider>
     </ContextOfState.Provider>)
 
