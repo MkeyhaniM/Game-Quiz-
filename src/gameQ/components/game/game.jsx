45308c266f7aa7890bb9fe32df9ab2e27033f5dc
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-09 12:32:42 -0800

    Include use Effect Context State

--- src/gameQ/components/game/game.jsx
+++ src/gameQ/components/game/game.jsx
@@ -1,29 +1,56 @@
-import React, {useState, createContext} from "react";
-import {HandelQuiz} from "./creatQuiz/HandelQuiz";
-import {Quiz} from "../../quiz/quiz";
-import {shuffle} from "./mixQuiz/mixQuestionAndAnswers"
+import { useState, createContext, useEffect } from "react";
+import { HandelQuiz } from "./creatQuiz/HandelQuiz";
+import { Quiz } from "../../quiz/quiz";
+import { shuffle } from "./mixQuiz/mixQuestionAndAnswers";
+import { useMemo } from "react";
+import { InEndOfGame } from "./creatQuiz/inEndOfGame/inEndOfGame";
+const arr = shuffle(Quiz);
 
-const arr = shuffle(Quiz)
-
-export const pro = createContext(null)
-
+export const pro = createContext(null);
 
 export function Game_quiz() {
     const [counterQuestion, setCounterQuestion] = useState(0);
-    const [selectedQuiz, setSelectedQuiz] = useState(arr[counterQuestion]); // selected a quiz from sh_array[counterQuestion]
+    const [SelectedQuiz, setSelectedQuiz] = useState(arr[counterQuestion]); // selected a quiz from sh_array[counterQuestion]
     const [sendInformationQuiz, setSendInformationQuiz] = useState({
-        selectedQuiz,
-        shuffledTests: shuffle(selectedQuiz.Test),
-        Answer: selectedQuiz.Answer
+        SelectedQuiz,
+        ShuffledTests: shuffle(SelectedQuiz.Test),
+        Answer: SelectedQuiz.Answer,
+        NumberOfQuiz: counterQuestion
     });
+
     function getNextQuiz() {
-        setCounterQuestion(prevState => prevState + 1)
+        setCounterQuestion(prev => prev + 1)
     }
 
+    useEffect(() => {
+        setSelectedQuiz(arr[counterQuestion])
+        setSendInformationQuiz({
+            SelectedQuiz,
+            ShuffledTests: shuffle(SelectedQuiz.Test),
+            Answer: SelectedQuiz.Answer,
+            NumberOfQuiz: counterQuestion
+        })
+    }, [counterQuestion, SelectedQuiz])
 
+    useMemo(() => {
+        if (counterQuestion > 10) {
+            <FirstValue />
+        }
+    }, [counterQuestion, SelectedQuiz])
+
+
+    function FirstValue() {
+        if (counterQuestion < 100) {
+            return (
+                <pro.Provider value={sendInformationQuiz}>
+                    <HandelQuiz GET_N_Q={getNextQuiz} qc={counterQuestion} />
+                </pro.Provider>
+            )
+        } else {
+            return <InEndOfGame />
+        }
+    }
     return (
-        <pro.Provider value={sendInformationQuiz}>
-            <HandelQuiz GET_N_Q={getNextQuiz} qc={counterQuestion}/>
-        </pro.Provider>
+        <FirstValue />
     );
 }
