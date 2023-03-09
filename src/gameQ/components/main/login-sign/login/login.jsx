cfa8de727d7c623d5c3a2448247b15fb4658914e
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-09 12:40:58 -0800

    Login form

--- src/gameQ/components/main/login-sign/login/login.jsx
+++ src/gameQ/components/main/login-sign/login/login.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
