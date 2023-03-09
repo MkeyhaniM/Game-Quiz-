a3a343b372cae28065ead7c1988bacac278dd4dc
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-09 12:33:29 -0800

    Game Quiz

--- src/gameQ/gameq.js
+++ src/gameQ/gameq.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { MainPage } from "./components/main/main-page";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+                
 export default function gameq() {
   return (
     <div className="container">
