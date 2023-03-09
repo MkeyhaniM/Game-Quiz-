75f56302876459e4476995599871237f21cd709c
mkeyhanim <mkeyhanideve@gmail.com>
2023-03-09 12:37:53 -0800

    Export score of user

--- src/gameQ/components/game/creatQuiz/score/score.jsx
+++ src/gameQ/components/game/creatQuiz/score/score.jsx
@@ -1,28 +1,26 @@
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 
-var scoreOfUser = 0;
+export var ScoreOfUser = 0;
 var lastChose = '';
 
-export function CheckTypeOfAnswer({type, score}) {
+export function CheckTypeOfAnswer({ type, score }) {
     if (score == 10) {
-        scoreOfUser += 10;
+        ScoreOfUser += 10;
     } else {
-        scoreOfUser += 0
+        ScoreOfUser += 0
     }
-    if (type == 'correctAnswer') {
-        lastChose = 'correct'
-    }
+    lastChose = type;
 }
 
 export function Score() {
     return (<>
         <div>
             <Button className='text-white mt-5 text-dark fw-semibold' variant="success">
-                Score : <Badge bg="success">{scoreOfUser}</Badge>
+                Score : <Badge bg="success">{ScoreOfUser}</Badge>
                 <span className="visually-hidden">unread messages</span>
             </Button>
-            <p className='bg-white mt-2'>it was - {lastChose == 'correct' ? "correct" : "wrong"} -</p>
+            <p className='bg-white mt-2'>The answer had been - {lastChose} {!lastChose && "Nothing !"} -</p>
         </div>
     </>);
 }
